388510fd9c16446c1876abbd5f75dc64045cabe9
jspinerola <julianspindola@gmail.com>
2024-11-04 19:23:11 -0600

    yessir

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
-import { useState } from "react";
 import "./App.css";
-import Navbar from "./components/Navbar";
-import Posts from "./components/Posts";
-import data from "./data.json"
 
 function App() {
-  const postCount = data.length
-
   return (
     <>
-      <Navbar />
-      <main>
-        <h2>Post Count: {postCount}</h2>
-        <Posts data={data} />
-      </main>
+    <div>Starting Project</div>
     </>
   );
 }
